a8ed93de4f9bfc3b5b74dd67cee45a0f36016a9c
Rahul <chrahulofficial@gmail.com>
2024-06-01 18:54:47 +0530

    dn

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.css";
 import { Donate, Home, Signin, Start } from "./pages";
+import { Route, Routes } from "react-router-dom";
+
 export default function App() {
   return (
-    <>
-      {/* <Home/> */}
-      {/* <Start /> */}
-      {/* <Signin /> */}
-      <Donate />
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/start" element={<Start />} />
+      <Route path="/Signin" element={<Signin />} />
+      <Route path="/Donate" element={<Donate />} />
+    </Routes>
   );
 }
